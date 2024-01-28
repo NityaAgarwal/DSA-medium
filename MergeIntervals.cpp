ecd7aace8ce6a3cdde4fda55d33ec8381619342d
NityaAgarwal <114755686+NityaAgarwal@users.noreply.github.com>
2024-01-29 04:20:34 +0530

    Create MergeIntervals.cpp

---
+++ MergeIntervals.cpp
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/merge-intervals/
+
+class Solution {
+public:
+    
+
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        int i = 1;
+        vector<vector<int>>ans;
+        while (i<intervals.size()) {
+            //check if they overlap
+            if (intervals[i][0] <= intervals[i-1][1]) {
+                //this means they overlap
+                //so they should be merged
+                intervals[i][0] = intervals[i-1][0];
+                intervals[i][1] = max(intervals[i-1][1], intervals[i][1]);
+            }
+            else ans.push_back(intervals[i-1]);
+            i++;
+        }
+        // push the last interval or the merged interval
+        ans.push_back(intervals.back());
+        return ans;
+    }
+};
